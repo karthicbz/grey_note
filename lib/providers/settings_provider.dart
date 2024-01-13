3b8aab29f9a7878f62ff0db0f5402a68b3716425
karthicbz <pkarthicbz@live.in>
2024-01-13 21:21:36 +0530

    save theme pref

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,10 +1,36 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
   bool dark = false;
 
-  void enableDarkTheme() {
+  SettingsProvider() {
+    initializeSettings();
+  }
+
+  void initializeSettings() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    if (pref.getString('settings') != null) {
+      Map<String, dynamic> data = jsonDecode(pref.getString('settings') ?? '');
+      dark = data['theme'];
+      notifyListeners();
+    } else {
+      dark = false;
+    }
+  }
+
+  void enableDarkTheme() async {
     dark = !dark;
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    if (pref.getString('settings') != null) {
+      Map<String, dynamic> data = jsonDecode(pref.getString('settings') ?? '');
+      data['theme'] = dark;
+      pref.setString('settings', jsonEncode(data));
+    }else{
+      pref.setString('settings', jsonEncode({'theme':dark}));
+    }
     notifyListeners();
   }
 }
