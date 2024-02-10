d2870ef62aa6cab3d58c8a8d1e2793b297f68368
karthicbz <pkarthicbz@live.in>
2024-02-10 15:56:26 +0530

    change navigation bar to default android

--- lib/pages/nav_page.dart
+++ lib/pages/nav_page.dart
@@ -14,42 +14,39 @@ class NavPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget currentWidget =
         context.watch<NavigationProvider>().currentPage ?? const Home();
+    int currentIndex = context.watch<NavigationProvider>().currentIndex;
+    String navigationBackground =
+        context.watch<SettingsProvider>().homeCardsColor;
     return Scaffold(
         body: AnimatedSwitcher(
           duration: const Duration(milliseconds: 500),
           child: currentWidget,
         ),
-        bottomNavigationBar: GNav(
-            gap: 8.0,
-            backgroundColor: Color(
-                int.parse(context.watch<SettingsProvider>().homeCardsColor)),
-            tabs: [
-              GButton(
-                icon: Icons.notes,
-                text: 'Notes',
-                haptic: true,
-                padding: const EdgeInsets.only(
-                  left: 90.0,
-                  right: 90.0,
-                  bottom: 16.0,
-                  top: 16.0,
-                ),
-                onPressed: () =>
-                    context.read<NavigationProvider>().notesTabClicked(),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            context.read<NavigationProvider>().changeDestination(index);
+          },
+          backgroundColor: Color(int.parse(navigationBackground)),
+          indicatorColor: Colors.white,
+          selectedIndex: currentIndex,
+          destinations: [
+            NavigationDestination(
+              icon: const Icon(Icons.notes),
+              label: 'Notes',
+              selectedIcon: Icon(
+                Icons.notes,
+                color: Color(int.parse(navigationBackground)),
               ),
-              GButton(
-                icon: Icons.done,
-                text: 'Todos',
-                haptic: true,
-                padding: const EdgeInsets.only(
-                  right: 90.0,
-                  left: 90.0,
-                  bottom: 16.0,
-                  top: 16.0,
-                ),
-                onPressed: () =>
-                    context.read<NavigationProvider>().todosTabClicked(),
-              )
-            ]));
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.check_box_outlined),
+              label: 'Notes',
+              selectedIcon: Icon(
+                Icons.check_box_outlined,
+                color: Color(int.parse(navigationBackground)),
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/providers/nav_provider.dart
+++ lib/providers/nav_provider.dart
@@ -4,14 +4,23 @@ import 'package:grey_note/pages/todos.dart';
 
 class NavigationProvider extends ChangeNotifier {
   Widget? currentPage;
+  int currentIndex = 0;
 
-  void notesTabClicked() {
-    currentPage = const Home();
-    notifyListeners();
-  }
+  // void notesTabClicked() {
+  //   currentPage = const Home();
+  //   notifyListeners();
+  // }
+  //
+  // void todosTabClicked() {
+  //   currentPage = const Todos();
+  //   notifyListeners();
+  // }
 
-  void todosTabClicked() {
-    currentPage = const Todos();
+  void changeDestination(int index) {
+    currentIndex = index;
+    (currentIndex == 0)
+        ? currentPage = const Home()
+        : currentPage = const Todos();
     notifyListeners();
   }
 }
